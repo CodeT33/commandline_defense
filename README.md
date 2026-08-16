dd87185d4894e25c9c0cdc73a2e1f3c8d28110cc
Tim Müller <tim.mueller05@web.de>
2026-08-16 17:24:46 +0200

    added GameMap struct

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # commandline_defense
 A video game where you have to defend against enemies while you build a factory using a commandline.
+
+---
+
+## Color codes
+
+`#00ffff` (aqua) path start
+`#ff00ff` (pink) path end
+`#ffff00` (yellow) path
+
+`#ff0000` (red) restricted
+`#00ff00` (green) placeable
+`#0000ff` (blue) water
--- map_parsing/src/lib.rs
+++ map_parsing/src/lib.rs
@@ -2,13 +2,44 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/**
+`#00ffff` (aqua) path start
+`#ff00ff` (pink) path end
+`#ffff00` (yellow) path
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+`#ff0000` (red) restricted
+`#00ff00` (green) placeable
+`#0000ff` (blue) water
+*/
+
+enum TileType {
+    PathStart,
+    PathEnd,
+    Path,
+    Restricted,
+    Placeable,
+    Water,
+}
+
+struct Vector2D {
+    x: u16,
+    y: u16,
+}
+
+struct Path {
+    path_corners: Vec<Vector2D>,
+}
+
+struct GameMap {
+    map_size: Vector2D,
+    tiles: Vec<TileType>,
+    path: Path,
+}
+
+fn get_index_from_map_position(map_position: Vector2D) -> usize {
+    (map_position.x * map_position.y + map_position.x) as usize
+}
+
+fn get_map_position_from_index(index: usize, map_size: Vector2D) -> Vector2D {
+    Vector2D{x: index as u16 % map_size.x, y: index as u16 / map_size.x}
 }
