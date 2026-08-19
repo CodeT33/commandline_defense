92e29d6d38eb37ec1e3fa62529caa30a13a3b369
Tim Müller <tim.mueller05@web.de>
2026-08-19 19:33:27 +0200

    adjusted the camera field so that the whole map is visible

--- src/camera.rs
+++ src/camera.rs
@@ -13,7 +13,6 @@ pub fn set_camera_position(
     let Ok(mut tf) = camera.single_mut() else {
         return;
     };
-
     tf.translation = Vec3::new(MAP_SIZE_TILES[0] as f32 / 2.0, MAP_SIZE_TILES[1] as f32 / 2.0, 0.0);
-    tf.scale = Vec3::splat((Vec2::from(MAP_SIZE_TILES.map(f32::from)) / window_size).max_element());
+    tf.scale = Vec3::splat((Vec2::from(MAP_SIZE_TILES.map(f32::from)) / window_size + 0.005).max_element());
 }
