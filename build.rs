f794a6268b70fbb13b15c0e1b7d3eb1573e1f1b4
Mika Wunderlich <mika.wunderlich2005@gmail.com>
2026-09-03 20:25:39 +0200

    remove unnecessary comments

--- build.rs
+++ build.rs
@@ -1,11 +1,9 @@
-// build.rs
 use std::fs;
 use std::path::Path;
 
 fn main() {
     println!("cargo:rerun-if-changed=assets");
 
-    // Place the stamp in a tracked subfolder (e.g., .cargo/ or assets/.stamp)
     let stamp_path = Path::new("assets/.stamp");
     let now =
         std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_nanos();
